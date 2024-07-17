84fe6754abb106ba2a04e301991dff8a35c61979
juyeong Kim <kjy47142@gmail.com>
2024-07-17 09:42:20 +0900

    ch6

--- textbook.ch6.py
+++ textbook.ch6.py
@@ -1,5 +1,6 @@
 #### 240716 ####
-import panddas as pd
+import pandas as pd
+import numpy as np
 
 # 데이터전처리 함수
 # query()
@@ -88,3 +89,62 @@ df.query('category == "suv"') \
   .agg(mean_tot = ('total', 'mean')) \
   .sort_values('mean_tot', ascending = False) \
   .head()
+
+
+
+
+#### 240717 ####
+## 데이터 합치기
+# 중간고사 데이터 만들기
+test1 = pd.DataFrame({'id' : [1,2,3,4,5],
+                      'midterm': [60,80,70,90,85]})
+test2 = pd.DataFrame({'id' : [1,2,3,40,5],
+                      'final': [70,83,65,95,80]})
+test1
+test2
+
+# left join
+total = pd.merge(test1, test2, how='left', on = 'id')
+total
+# 다른 아이디가 있으면 nan값으로 표시됨
+
+# inner join(교집합)
+total = pd.merge(test1, test2, how='inner', on = 'id')
+total
+
+# outer join(합집합)
+total = pd.merge(test1, test2, how='outer', on = 'id')
+total
+
+
+# 다른 데이터를 활용해 변수 추가하기
+name = pd.DataFrame({'nclass' : [1,2,3,4,5],
+                     'teacher' : ['kim', 'lee', 'park', 'choi', 'jung']})
+name
+
+exam = pd.read_csv("data/exam.csv")
+exam
+
+exam_new = pd.merge(exam, name, how='left', on = 'nclass')
+exam_new
+
+
+# 세로로 합치기
+score1 = pd.DataFrame({'id' : [1,2,3,4,5],
+                      'score': [60,80,70,90,85]})
+score2 = pd.DataFrame({'id' : [6,7,8,9,10],
+                      'score': [70,83,65,95,80]})
+score1
+score2
+
+score_all = pd.concat([score1, score2])
+score_all
+# 인덱스 차례대로: score_all= score_all.reset_index(drop=True)
+
+# 열로 합치기    
+test1 = pd.DataFrame({'id' : [1,2,3,4,5],
+                      'midterm': [60,80,70,90,85]})
+test2 = pd.DataFrame({'id' : [1,2,3,40,5],
+                      'final': [70,83,65,95,80]})
+                      
+pd.concat([test1,test2], axis=1)
